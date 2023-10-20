e1f8eaf48d070ac8963e3ac6acaed6dba1082235
L41se <manh.kien.21122012@gmail.com>
2023-10-20 16:25:40 +0700

    origin

---
+++ SimpleLinkedList/Test.java
@@ -0,0 +1,14 @@
+package SimpleLinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.printList();
+    }
+}
